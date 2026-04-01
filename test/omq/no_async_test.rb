ceb806540312d0ccee16eade96e4403e2f453dca
Patrik Wenger <paddor@gmail.com>
2026-04-01 02:17:04 +0200

    add test for linger unregistration on close

--- test/omq/no_async_test.rb
+++ test/omq/no_async_test.rb
@@ -16,4 +16,24 @@ describe "non-Async usage" do
     push&.close
     pull&.close
   end
+
+
+  it "unregisters linger when a socket is closed before shutdown" do
+    a = OMQ::PUSH.new(nil, linger: 0)
+    b = OMQ::PUSH.new(nil, linger: 0)
+    a.bind("tcp://127.0.0.1:0")
+    b.bind("tcp://127.0.0.1:0")
+
+    lingers = OMQ::Reactor.instance_variable_get(:@lingers)
+    assert_equal 2, lingers[0]
+
+    a.close
+    assert_equal 1, lingers[0]
+
+    b.close
+    assert_equal 0, lingers.fetch(0, 0)
+  ensure
+    a&.close
+    b&.close
+  end
 end
